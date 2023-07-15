d6d543d779d3420bdbb61d5397c3b9938139796b
soheil alimadadi <soheilblizzard@gmail.com>
2023-07-15 16:09:08 +0330

    feat(auth): add user model

---
+++ auth/models/user.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from beanie import (
+    Document,
+    before_event,
+    Insert
+)
+from pydantic import (
+    Field,
+    EmailStr
+)
+
+class User(Document):
+    """
+    A data model representing a user in the database.
+
+    Attributes:
+        username (EmailStr): The user's email address.
+        password (str): The user's password, hashed for security.
+
+    Settings:
+        name (str): The name of the database collection for User documents.
+
+    Methods:
+        __str__() -> str:
+            Returns a string representation of the User object.
+        __repr__() -> str:
+            Returns a string representation of the User object for debugging purposes.
+    """
+    username: EmailStr = Field(
+        description="Email of the user",
+        example="example@gmail.com"
+    )
+    password: str = Field(
+        description="Password of the user",
+        example="$2b$12$OVZbIYJqsiyP.5NUOdrNUOLFC7l08dpN5DC7lG8VhP4A.nK/NI7Cy"
+    )
+
+    created: datetime = Field(
+        description="User creation time"
+    )
+
+    class Settings:
+        name = "user_db"
+
+    def __str__(self):
+        return f"{self.username}"
+
+    def __repr__(self):
+        return f"<user: {self.username}>"
+
+    @before_event(Insert)
+    def completed_on(self):
+        self.created = datetime.now()
