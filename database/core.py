e16d18576220b7f5bc2898e46ad0e8bba2ef5ed6
soheil alimadadi <soheilblizzard@gmail.com>
2023-07-15 19:34:58 +0330

    feat(tasks): implement task document model

--- database/core.py
+++ database/core.py
@@ -3,6 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from kernel.settings import DATABASE_URL
 from auth.models import User
+from tasks.models import Task
 
 
 async def init_db():
@@ -10,4 +11,4 @@ async def init_db():
     client = AsyncIOMotorClient(DATABASE_URL)
 
     # Initialize beanie with the Product document class and a database
-    await init_beanie(database=client.twt_db, document_models=[User])
+    await init_beanie(database=client.todo_db, document_models=[User, Task])
---
+++ tasks/models/task.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import (
+    Field,
+    EmailStr
+)
+from beanie import (
+    Document,
+    before_event,
+    Insert
+)
+
+
+class Task(Document):
+    """
+    A Pydantic model representing a task object in the database.
+
+    Attributes:
+        title (str): The title of the task.
+        description (Optional[str]): The description of the task.
+        is_completed (bool): Whether the task is completed or not.
+        user (EmailStr): The email address of the user who created the task.
+        created (datetime): The date and time when the task was created.
+        completed_on (Optional[datetime]): The date and time when the task
+        was completed, if it is completed.
+    """
+    title: str = Field(
+        min_length=4,
+        max_length=150,
+        description="Title of the task",
+    )
+    description: Optional[str] = Field(
+        default=None,
+        description="Description of the task"
+    )
+    is_completed: bool = Field(
+        default=False,
+        description="Whether the task is completed or not"
+    )
+    user: EmailStr = Field(
+        description="Email of the user who created this task"
+    )
+    created: datetime = Field(
+        description="Task creation time"
+    )
+    completed_on: Optional[datetime] = None
+
+    class Settings:
+        name = "task_db"
+        validate_on_save = True
+
+    @before_event(Insert)
+    def completed_on(self):
+        self.created = datetime.now()
+
+    @before_event(Insert)
+    def lower_name(self):
+        self.title = self.title.lower()
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def __repr__(self):
+        return f"<task: {self.title} - user: {self.user}>"
